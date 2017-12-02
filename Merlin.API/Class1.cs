b53c3babd562bec1472fdfd15eded9ed4ceb24cf
Lukáš Jech <klukule@moowdesign.eu>
2017-12-02 19:03:03 +0100

    Basic project structure

---
+++ Merlin.API/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merlin.API
+{
+    public class Class1
+    {
+        //Here will be whole API
+    }
+}
---
+++ Merlin.Hook/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merlin.Hook
+{
+    public class Class1
+    {
+        //This will be hook itself... (concurent task manager, script manager, logging etc....)
+    }
+}
